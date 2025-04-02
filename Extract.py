b41c1d788324c9541d15c843b24844b5ea9bf80e
Ebrahim Emad <ebemad8@gmail.com>
2025-04-02 16:40:23 +0200

    edit the code

--- Extract.py
+++ Extract.py
@@ -3,35 +3,32 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import os
 
-def get_data(output_dir='data',output_filename="world_population.csv"):
+def get_data(output_dir='data'):
     try:
-        os.makedirs(output_dir,exist_ok=True)
-        output_path=os.path.join(output_dir,output_filename)
-        if os.path.exists(output_path):
-            print(f"{output_path} already exists , skipping downloading")
+        
+        if os.path.exists(output_dir):
+            print(f"{output_dir} already exists , skipping downloading")
             return None
+        os.makedirs(output_dir,exist_ok=True)
         url = "https://www.worldometers.info/world-population/population-by-country/"
         response = requests.get(url)
         response.raise_for_status()  # Check for HTTP errors
         soup = BeautifulSoup(response.text, 'html.parser')
-        # Find the table (assuming it's the first table on the page)
-        table = soup.find('table')
-        # Extract table rows
-        rows = table.find_all('tr')
-        # Extract headers (column names)
-        headers = [th.text.strip() for th in rows[0].find_all('th')]
-        # Extract data rows
-        data = []
-        for row in rows[1:]:
-            cols = row.find_all('td')
-            data.append([col.text.strip() for col in cols])
-        
-        # Convert to DataFrame and save as CSV
-        df = pd.DataFrame(data, columns=headers)
-        df.to_csv(output_path, index=False)
-        
-        print(f"Data successfully saved to {output_filename}!")
-        return df
+        # Find ALL tables
+        tables = soup.find_all('table')
+        # Loop through each table and save as CSV
+        for i ,table in enumerate(tables,start=1):
+            headers=[th.text.strip() for th in table.find_all('th')]
+            # Extract data rows
+            data=[]
+            for row in table.find_all('tr')[1:]:
+                cols=row.find_all('td')
+                data.append([col.text.strip() for col in cols])
+            df=pd.DataFrame(data,columns=headers)
+            output_path=os.path.join(output_dir,f'table_{i}.csv')
+            df.to_csv(output_path,index=False)
+            print(f"Table {i} saved to: {output_path}")
+        print(f"\nAll {len(tables)} tables saved to '{output_dir}'!")
     
     except Exception as e:
         raise Exception(f"ERROR: {e}")
