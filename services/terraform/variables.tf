b8840656106b55846d97fbc5092dbb9a49e3a08e
Ebrahim Emad <ebemad8@gmail.com>
2025-12-14 11:33:47 +0200

    update tf

--- services/terraform/variables.tf
+++ services/terraform/variables.tf
@@ -1,34 +1,57 @@
-variable "credentials" {
-  description = "Path to Google Cloud service account credentials"
-  default     = "./keys/my-creds.json"
+# AWS Configuration Variables
+variable "aws_region" {
+  description = "AWS region for resources"
+  type        = string
+  default     = "me-south-1" # Bahrain (closest to Egypt)
 }
 
-variable "location" {
-  description = "Google Cloud project location"
-  default     = "US"
+variable "aws_profile" {
+  description = "AWS profile to use (optional)"
+  type        = string
+  default     = "default"
 }
 
-variable "project" {
-  description = "Google Cloud project ID"
-  default     = "sql2-456517"
+variable "project_name" {
+  description = "Name of the project"
+  type        = string
+  default     = "world-population-etl"
 }
 
-variable "region" {
-  description = "Google Cloud region"
-  default     = "us-central1"
+variable "environment" {
+  description = "Environment name (dev, staging, prod)"
+  type        = string
+  default     = "dev"
 }
 
-variable "bq_dataset_name" {
-  description = "BigQuery dataset name"
-  default     = "demo_dataset"
+# S3 Configuration
+variable "s3_bucket_name" {
+  description = "Name of the S3 bucket for data storage"
+  type        = string
+  default     = "world-population-etl-bucket"
 }
 
-variable "gcs_bucket_name" {
-  description = "Google Cloud Storage bucket name"
-  default     = "artful-palace-451517-f8"
-}
-
-variable "gcs_storage_class" {
-  description = "Storage class for GCS bucket"
+variable "s3_storage_class" {
+  description = "Storage class for S3 bucket"
+  type        = string
   default     = "STANDARD"
+}
+
+# Glue Configuration
+variable "glue_database_name" {
+  description = "Name of the Glue database"
+  type        = string
+  default     = "world_population_db"
+}
+
+variable "glue_catalog_table_name" {
+  description = "Name of the Glue catalog table"
+  type        = string
+  default     = "population_data"
+}
+
+# IAM Configuration
+variable "create_iam_roles" {
+  description = "Whether to create IAM roles for Glue and other services"
+  type        = bool
+  default     = true
 }
